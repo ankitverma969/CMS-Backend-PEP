b6a351cfb2935e8e67e484e92e397c7e72aec7dc
ankitverma969 <blogankitverma@gmail.com>
2026-02-11 15:03:10 +0530

    Rate - Limiter Integration

--- routes/artifact.routes.js
+++ routes/artifact.routes.js
@@ -6,13 +6,13 @@ import {
 import { authMiddleware} from "../middleware/auth.middleware.js";
 import { authorizeRoles } from "../middleware/role.middleware.js";
 import { upload } from "../middleware/uploads.middleware.js";
-
+import apiLimiter from "../middleware/rate.limit.middleware.js";
 
 const router = express.Router();
 
 router.post("/create",authMiddleware,createArtifact);
 router.post("/createWithFile",authMiddleware,upload.single("file"), createArtifact);
-router.get("/", authMiddleware, getArtifacts);
+router.get("/", authMiddleware, apiLimiter, getArtifacts);
 router.get("/", authMiddleware,authorizeRoles("ADMIN"), getArtifacts);
 
 
